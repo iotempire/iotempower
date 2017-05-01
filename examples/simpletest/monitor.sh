e85f66e6fa1e74a8228f75beaf1ef4c9bb120435
Ulrich Norbisrath <devel@mail.ulno.net>
2017-05-01 07:31:56 -0400

    fixed usage of tempfile

--- examples/simpletest/monitor.sh
+++ examples/simpletest/monitor.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 topic="test1"
+file1="$HOME/tmp/temp1-new.log"
+file2="$HOME/tmp/temp2-new.log"
 #mosquitto_sub  -h 192.168.12.1 -t "$topic" | \
-#    ts "%s" | tee -a "$HOME/tmp/$1.log"
-mosquitto_sub  -h 192.168.12.1 -t "$topic/ht1/temperature" | \
-    ts "%s" > "$HOME/tmp/temp1.log" &
-id1=$!
+#    ts "%.s" | tee -a "$HOME/tmp/$1.log"
+#mosquitto_sub  -h 192.168.12.1 -t "$topic/ht1/temperature" | \
+#    ts "%.s" > "$file1" &
+#id1=$!
 mosquitto_sub  -h 192.168.12.1 -t "$topic/t2" | \
-    ts "%s" | tee -a "$HOME/tmp/temp2.log" &
+    ts "%.s" > "$file2" &
 id2=$!
+echo "Monitoring to $file2."
 echo "Press return or ctrl-c to finish."
 read tmp
 
