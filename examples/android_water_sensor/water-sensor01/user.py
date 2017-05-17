ee820f67f4e6ad4ff3841c6e726c1b004e8c803b
Ulrich Norbisrath <devel@mail.ulno.net>
2017-05-17 18:09:32 -0400

    onboadled fixes, more on water-sensor

--- examples/android_water_sensor/water-sensor01/user.py
+++ examples/android_water_sensor/water-sensor01/user.py
@@ -1,12 +1,12 @@
 from ulnoiot import *
 
 def main():
-    mqtt("192.168.12.1", "pipe07/wsens01")
+    mqtt("192.168.12.144", "pipe07/wsens01")
 
-    from ulnoiot.shield import onboardled as onboard
-    onboard.blue.high() # make sure it's off
+    from ulnoiot.shield.onboardled import blue
+    blue.high() # make sure it's off
 
-    input("water",d2,"1","0")
+    input("water",d2,"1","0",threshold=10)
 
     ## start to transmit every 10 seconds (or when status changed)
     run(10)
