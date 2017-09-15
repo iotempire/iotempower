b9fedc4a422793435a3564c233a9b445cff95d0b
Ulrich Norbisrath <ulno@ulno-x230.ulno.home>
2017-09-15 14:46:54 -0400

    removed psyco, xrange->range, comment all explanations

--- lib/chacha/chacha.py
+++ lib/chacha/chacha.py
@@ -31,84 +31,77 @@
 
 """
 
-import struct
-try:
-    import psyco            # a specializing [runtime] compiler
-    have_psyco = True       # for 32-bit architectures
-    print 'psyco enabled'
-except:
-    have_psyco = False
     
 #-----------------------------------------------------------------------
 
 class ChaCha(object):
-    """
-        ChaCha is an improved variant of Salsa20.
-        
-        Salsa20 was submitted to eSTREAM, an EU stream cipher
-        competition.  Salsa20 was originally defined to be 20
-        rounds.  Reduced round versions, Salsa20/8 (8 rounds) and
-        Salsa20/12 (12 rounds), were later submitted.  Salsa20/12
-        was chosen as one of the winners and 12 rounds was deemed
-        the "right blend" of security and efficiency.  Salsa20 
-        is about 3x-4x faster than AES-128.
-        
-        Both ChaCha and Salsa20 accept a 128-bit or a 256-bit key 
-        and a 64-bit IV to set up an initial 64-byte state.  For 
-        each 64-bytes of data, the state gets scrambled and XORed 
-        with the previous state.  This new state is then XORed 
-        with the input data to produce the output.  Both being 
-        stream ciphers, their encryption and decryption functions 
-        are identical.  
-        
-        While Salsa20's diffusion properties are very good, some 
-        claimed the IV/keystream correlation was too strong for 
-        comfort.  To satisfy, another variant called XSalsa20 
-        implements a 128-bit IV.  For the record, EU eSTREAM team 
-        did select Salsa20/12 as a solid cipher providing 128-bit 
-        security.  
-        
-        ChaCha is a minor tweak of Salsa20 that significantly 
-        improves its diffusion per round.  ChaCha is more secure 
-        than Salsa20 and 8 rounds of ChaCha, aka ChaCha8, provides 
-        128-bit security.  (FWIW, I have not seen any calls for a 
-        128-bit IV version of ChaCha or XChaCha.)  
-        
-        Another benefit is that ChaCha8 is about 5-8% faster than 
-        Salsa20/8 on most 32- and 64-bit PPC and Intel processors.  
-        SIMD machines should see even more improvement.  
-        
-        Sample usage:
-          from chacha import ChaCha
-          
-          cc8 = ChaCha(key, iv)
-          ciphertext = cc8.encrypt(plaintext)
-          
-          cc8 = ChaCha(key, iv)
-          plaintext = cc8.decrypt(ciphertext)
-        
-        Remember, the purpose of this program is educational; it 
-        is NOT a secure implementation nor is a pure Python version 
-        going to be fast.  Encrypting large data will be less than 
-        satisfying.  Also, no effort is made to protect the key or 
-        wipe critical memory after use.  
-        
-        Note that psyco, a specializing compiler somewhat akin to 
-        a JIT, can provide a 2x+ performance improvement over 
-        vanilla Python 32-bit architectures.  A 64-bit version of 
-        psyco does not exist.  See http://psyco.sourceforge.net
-        
-        For more information about Daniel Bernstein's ChaCha 
-        algorithm, please see http://cr.yp.to/chacha.html
-        
-        All we need now is a keystream AND authentication in the 
-        same pass.  
-        
-        Larry Bugbee
-        May 2009     (Salsa20)
-        August 2009  (ChaCha)
-        rev June 2010
-    """
+    # """
+    #     ChaCha is an improved variant of Salsa20.
+    #
+    #     Salsa20 was submitted to eSTREAM, an EU stream cipher
+    #     competition.  Salsa20 was originally defined to be 20
+    #     rounds.  Reduced round versions, Salsa20/8 (8 rounds) and
+    #     Salsa20/12 (12 rounds), were later submitted.  Salsa20/12
+    #     was chosen as one of the winners and 12 rounds was deemed
+    #     the "right blend" of security and efficiency.  Salsa20
+    #     is about 3x-4x faster than AES-128.
+    #
+    #     Both ChaCha and Salsa20 accept a 128-bit or a 256-bit key
+    #     and a 64-bit IV to set up an initial 64-byte state.  For
+    #     each 64-bytes of data, the state gets scrambled and XORed
+    #     with the previous state.  This new state is then XORed
+    #     with the input data to produce the output.  Both being
+    #     stream ciphers, their encryption and decryption functions
+    #     are identical.
+    #
+    #     While Salsa20's diffusion properties are very good, some
+    #     claimed the IV/keystream correlation was too strong for
+    #     comfort.  To satisfy, another variant called XSalsa20
+    #     implements a 128-bit IV.  For the record, EU eSTREAM team
+    #     did select Salsa20/12 as a solid cipher providing 128-bit
+    #     security.
+    #
+    #     ChaCha is a minor tweak of Salsa20 that significantly
+    #     improves its diffusion per round.  ChaCha is more secure
+    #     than Salsa20 and 8 rounds of ChaCha, aka ChaCha8, provides
+    #     128-bit security.  (FWIW, I have not seen any calls for a
+    #     128-bit IV version of ChaCha or XChaCha.)
+    #
+    #     Another benefit is that ChaCha8 is about 5-8% faster than
+    #     Salsa20/8 on most 32- and 64-bit PPC and Intel processors.
+    #     SIMD machines should see even more improvement.
+    #
+    #     Sample usage:
+    #       from chacha import ChaCha
+    #
+    #       cc8 = ChaCha(key, iv)
+    #       ciphertext = cc8.encrypt(plaintext)
+    #
+    #       cc8 = ChaCha(key, iv)
+    #       plaintext = cc8.decrypt(ciphertext)
+    #
+    #     Remember, the purpose of this program is educational; it
+    #     is NOT a secure implementation nor is a pure Python version
+    #     going to be fast.  Encrypting large data will be less than
+    #     satisfying.  Also, no effort is made to protect the key or
+    #     wipe critical memory after use.
+    #
+    #     Note that psyco, a specializing compiler somewhat akin to
+    #     a JIT, can provide a 2x+ performance improvement over
+    #     vanilla Python 32-bit architectures.  A 64-bit version of
+    #     psyco does not exist.  See http://psyco.sourceforge.net
+    #
+    #     For more information about Daniel Bernstein's ChaCha
+    #     algorithm, please see http://cr.yp.to/chacha.html
+    #
+    #     All we need now is a keystream AND authentication in the
+    #     same pass.
+    #
+    #     Larry Bugbee
+    #     May 2009     (Salsa20)
+    #     August 2009  (ChaCha)
+    #     rev June 2010
+    # """
 
     TAU    = ( 0x61707865, 0x3120646e, 0x79622d36, 0x6b206574 )
     SIGMA  = ( 0x61707865, 0x3320646e, 0x79622d32, 0x6b206574 )
@@ -117,18 +110,18 @@ class ChaCha(object):
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     def __init__(self, key, iv, rounds=ROUNDS):
-        """ Both key and iv are byte strings.  The key must be 
-            exactly 16 or 32 bytes, 128 or 256 bits respectively.  
-            The iv must be exactly 8 bytes (64 bits) and MUST 
-            never be reused with the same key.
-            
-            The default number of rounds is 8.
-
-            If you have several encryptions/decryptions that use 
-            the same key, you may reuse the same instance and 
-            simply call iv_setup() to set the new iv.  The previous 
-            key and the new iv will establish a new state.
-        """
+        # """ Both key and iv are byte strings.  The key must be
+        #     exactly 16 or 32 bytes, 128 or 256 bits respectively.
+        #     The iv must be exactly 8 bytes (64 bits) and MUST
+        #     never be reused with the same key.
+        #
+        #     The default number of rounds is 8.
+        #
+        #     If you have several encryptions/decryptions that use
+        #     the same key, you may reuse the same instance and
+        #     simply call iv_setup() to set the new iv.  The previous
+        #     key and the new iv will establish a new state.
+        # """
         self._key_setup(key)
         self.iv_setup(iv)
         self.rounds = rounds
@@ -136,12 +129,12 @@ class ChaCha(object):
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     def _key_setup(self, key):
-        """ key is converted to a list of 4-byte unsigned integers
-            (32 bits).
-
-            Calling this routine with a key value effectively resets
-            the context/instance.  Be sure to set the iv as well.
-        """
+        # """ key is converted to a list of 4-byte unsigned integers
+        #     (32 bits).
+        #
+        #     Calling this routine with a key value effectively resets
+        #     the context/instance.  Be sure to set the iv as well.
+        # """
         if len(key) not in [16, 32]:
             raise Exception('key must be either 16 or 32 bytes')
         key_state = [0]*16
@@ -183,20 +176,20 @@ class ChaCha(object):
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     def iv_setup(self, iv):
-        """ self.state and other working structures are lists of
-            4-byte unsigned integers (32 bits).
-
-            The iv is not a secret but it should never be reused 
-            with the same key value.  Use date, time or some other
-            counter to be sure the iv is different each time, and
-            be sure to communicate the IV to the receiving party.
-            Prepending 8 bytes of iv to the ciphertext is the usual
-            way to do this.
-
-            Just as setting a new key value effectively resets the
-            context, setting the iv also resets the context with
-            the last key value entered.
-        """
+        # """ self.state and other working structures are lists of
+        #     4-byte unsigned integers (32 bits).
+        #
+        #     The iv is not a secret but it should never be reused
+        #     with the same key value.  Use date, time or some other
+        #     counter to be sure the iv is different each time, and
+        #     be sure to communicate the IV to the receiving party.
+        #     Prepending 8 bytes of iv to the ciphertext is the usual
+        #     way to do this.
+        #
+        #     Just as setting a new key value effectively resets the
+        #     context, setting the iv also resets the context with
+        #     the last key value entered.
+        # """
         if len(iv) != 8:
             raise Exception('iv must be 8 bytes')
         v = list(struct.unpack('<2I', iv))
@@ -212,21 +205,21 @@ class ChaCha(object):
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     def encrypt(self, datain):
-        """ Encrypt a chunk of data.  datain and the returned value 
-            are byte strings.
-
-            If large data is submitted to this routine in chunks,
-            the chunk size MUST be an exact multiple of 64 bytes.
-            Only the final chunk may be less than an even multiple.
-            (This function does not "save" any uneven, left-over 
-            data for concatenation to the front of the next chunk.)
-            
-            The amount of available memory imposes a poorly defined
-            limit on the amount of data this routine can process.
-            Typically 10's and 100's of KB are available but then,
-            perhaps not.  This routine is intended for educational 
-            purposes so application developers should take heed.
-        """
+        # """ Encrypt a chunk of data.  datain and the returned value
+        #     are byte strings.
+        #
+        #     If large data is submitted to this routine in chunks,
+        #     the chunk size MUST be an exact multiple of 64 bytes.
+        #     Only the final chunk may be less than an even multiple.
+        #     (This function does not "save" any uneven, left-over
+        #     data for concatenation to the front of the next chunk.)
+        #
+        #     The amount of available memory imposes a poorly defined
+        #     limit on the amount of data this routine can process.
+        #     Typically 10's and 100's of KB are available but then,
+        #     perhaps not.  This routine is intended for educational
+        #     purposes so application developers should take heed.
+        # """
         if self.lastblock_sz != 64:
             raise Exception('last chunk size not a multiple of 64 bytes')
         dataout = []
@@ -257,13 +250,13 @@ class ChaCha(object):
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     
     def _chacha_scramble(self):     # 64 bytes in
-        """ self.state and other working strucures are lists of
-            4-byte unsigned integers (32 bits).
-
-            output must be converted to bytestring before return.
-        """
+        # """ self.state and other working strucures are lists of
+        #     4-byte unsigned integers (32 bits).
+        #
+        #     output must be converted to bytestring before return.
+        # """
         x = self.state[:]           # makes a copy
-        for i in xrange(0, self.rounds, 2):
+        for i in range(0, self.rounds, 2):
             # two rounds per iteration
             self._quarterround(x, 0, 4, 8,12)
             self._quarterround(x, 1, 5, 9,13)
@@ -275,7 +268,7 @@ class ChaCha(object):
             self._quarterround(x, 2, 7, 8,13)
             self._quarterround(x, 3, 4, 9,14)
             
-        for i in xrange(16):
+        for i in range(16):
             x[i] = (x[i] + self.state[i]) & 0xffffffff
         output = struct.pack('<16I',
                             x[ 0], x[ 1], x[ 2], x[ 3],
@@ -286,24 +279,24 @@ class ChaCha(object):
     
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     
-    '''
-    # as per definition - deprecated
-    def _quarterround(self, x, a, b, c, d):
-        x[a] = (x[a] + x[b]) & 0xFFFFFFFF
-        x[d] = self._rotate((x[d]^x[a]), 16)
-        x[c] = (x[c] + x[d]) & 0xFFFFFFFF
-        x[b] = self._rotate((x[b]^x[c]), 12)
-        
-        x[a] = (x[a] + x[b]) & 0xFFFFFFFF
-        x[d] = self._rotate((x[d]^x[a]), 8)
-        x[c] = (x[c] + x[d]) & 0xFFFFFFFF
-        x[b] = self._rotate((x[b]^x[c]), 7)
-        
-    def _rotate(self, v, n):        # aka ROTL32
-        return ((v << n) & 0xFFFFFFFF) | (v >> (32 - n))
-    '''
-    
-    # surprisingly, the following tweaks/accelerations provide 
+    # '''
+    # # as per definition - deprecated
+    # def _quarterround(self, x, a, b, c, d):
+    #     x[a] = (x[a] + x[b]) & 0xFFFFFFFF
+    #     x[d] = self._rotate((x[d]^x[a]), 16)
+    #     x[c] = (x[c] + x[d]) & 0xFFFFFFFF
+    #     x[b] = self._rotate((x[b]^x[c]), 12)
+    #
+    #     x[a] = (x[a] + x[b]) & 0xFFFFFFFF
+    #     x[d] = self._rotate((x[d]^x[a]), 8)
+    #     x[c] = (x[c] + x[d]) & 0xFFFFFFFF
+    #     x[b] = self._rotate((x[b]^x[c]), 7)
+    #
+    # def _rotate(self, v, n):        # aka ROTL32
+    #     return ((v << n) & 0xFFFFFFFF) | (v >> (32 - n))
+    # '''
+
+    # surprisingly, the following tweaks/accelerations provide
     # about a 20-40% gain
     def _quarterround(self, x, a, b, c, d):
         xa = x[a]
@@ -333,27 +326,10 @@ class ChaCha(object):
     
     def _xor(self, stream, datain):
         dataout = []
-        for i in xrange(min(len(stream), len(datain))):
+        for i in range(min(len(stream), len(datain))):
             dataout.append(chr(ord(stream[i])^ord(datain[i])))
         return ''.join(dataout)
     
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    
-    if have_psyco:
-        # if you psyco encrypt() and _chacha_scramble() you
-        # should get a 2.4x speedup over vanilla Python 2.5.  
-        # The other functions seem to offer only negligible 
-        # improvement.  YMMV.
-        
-        _key_setup = psyco.proxy(_key_setup)    # small impact
-        iv_setup   = psyco.proxy(iv_setup)      # small impact
-        encrypt    = psyco.proxy(encrypt)                   # 18-32%
-        _chacha_scramble = psyco.proxy(_chacha_scramble)    # big help, 2x
-        _quarterround    = psyco.proxy(_quarterround)       # ???
-#        _rotate = psyco.proxy(_rotate)          # minor impact
-        _xor    = psyco.proxy(_xor)             # very small impact
-        pass
-
 #-----------------------------------------------------------------------
 #-----------------------------------------------------------------------
 #-----------------------------------------------------------------------
