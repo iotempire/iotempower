42d1ee4b375ac713e4f6d9d9d88df2e6a35a69cd
Ulrich Norbisrath <ulno@ulno-x230.ulno.home>
2017-09-25 14:17:15 -0400

    disable most things by default in template to force prior interaction by user

--- lib/system_template/node_template/files/autostart.py
+++ lib/system_template/node_template/files/autostart.py
@@ -15,18 +15,18 @@ from ulnoiot import *
 # The onboard-led is always available.
 # With this configuration it will report under myroom/test1/blue
 # and can be set via sending off or on to myroom/test1/blue/test.
-from ulnoiot.shield.onboardled import blue
-blue.high() # make sure it's off (it's reversed)
+#from ulnoiot.shield.onboardled import blue
+#blue.high() # make sure it's off (it's reversed)
 
-## Add some other devices
+## Add some other devices (all commented out)
 # Add a button with a slightly higher debounce rate, which will report
 # in the topic myroom/test1/button1.
-button("button1",d8,threshold=2)
+#button("button1",d8,threshold=2)
 
 # Count rising signals on d2=Pin(4) and
 # report number counted at myroom/test1/shock1.
-trigger("shock1",Pin(4))
+#trigger("shock1",Pin(4))
 
 ## Start to transmit every 10 seconds (or when status changed).
-# Don't forget the run-comamnd at the end.
-run(10)
+# Don't forget (uncomment!) the run-comamnd at the end.
+#run(10)
