15f6673c2f96de154e82662fb59796aed3d7ee42
Ulrich Norbisrath <devel@mail.ulno.net>
2017-12-28 09:25:45 -0500

    sudo execute scripts

---
+++ examples/lightboard/run-main.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo bash -c ". $ULNOIOT_VPYTHON/bin/activate; PYTHONPATH=$ULNOIOT_ROOT/lib python main.py"
---
+++ examples/lightboard/run-test.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo $(which python) lightboard_test.py "/home/pi/dots matrix test.png"
