613084ff9196845ccbef2cb5e2c8e1ea229ee7ec
Ulrich Norbisrath <devel@mail.ulno.net>
2017-12-29 14:02:54 -0500

    more features for raspi gateway boot

--- lib/system_boot/raspi_boot.sh
+++ lib/system_boot/raspi_boot.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
+# Run a raspberry pi as ulnoiot gateway (wifi router and mqtt_broker)
+#
+# To enable this, add the following to the end of /etc/rc.local with adjusted ULNOIOT_ROOT:
+# export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+# export ULNOIOT_ROOT="/home/pi/ulnoiot"
+# source "$ULNOIOT_ROOT/lib/shell_starter/ulnoiot.bash"
+# "$ULNOIOT_ROOT/lib/system_boot/raspi_boot.sh"
+#
+# Also disable all network devices in /etc/network/interfaces apart lo and wlan1
+# and make sure that wlan1 configuration looks like this:
+# allow-hotplug wlan1
+# iface wlan1 inet manual
+#    wpa-conf /usr/local/etc/uiot_wpa_supplicant.conf
+
+
+[ "$ULNOIOT_ACTIVE" = "yes" ] || { echo "ulnoiot not active, aborting." 1>&2;exit 1; }
 
 # check if otg-mode was enabled
 egrep '^\s*dtoverlay=dwc2\s*' /boot/config.txt && otg_on=1
@@ -15,7 +31,9 @@ if [[ ! "$otg_on" = 1 ]]; then
         # without extra certificate (for example to an eduroam university network)
         # else try to connect to wpa network
         cfg="/usr/local/etc/uiot_wpa_supplicant.conf"
-        cp "$cfg.orig" "$cfg"
+        cp "$ULNOIOT_ROOT/etc/ulnoiot_wpa_supplicant.conf.base" "$cfg"
+        chown root.root "$cfg"
+        chmod 600 "$cfg"
         if [[ "$uiot_wifi_user" ]]; then # a user was given, so try enterprise network
 
 cat << EOF >> "$cfg"
@@ -26,7 +44,6 @@ scan_ssid=1
 eap=TTLS
 identity="$uiot_wifi_user"
 password="$uiot_wifi_password"
-password="password"
 phase1="peaplabel=0"
 phase2="auth=MSCHAPV2"
 key_mgmt=WPA-PSK
@@ -58,8 +75,23 @@ if [[ "$uiot_ap_password" ]]; then # pw was given, so start an accesspoint
         uiot_ap_name=iotempire
     fi
 
-    # TODO: start accesspoint
+    # overwrite some configuration variables
+    #ULNOIOT_AP_DEVICES="wlan1 wlan0"
+
+    # Name of accesspoint to create
+    export ULNOIOT_AP_NAME="$uiot_ap_name"
+
+    if [[ "$add_id" = 1 ]]; then
+        export ULNOIOT_AP_ADDID=yes
+    else
+        export ULNOIOT_AP_ADDID=no
+    fi
+
+    ULNOIOT_AP_PASSWORD="$uiot_ap_password"
+
+    # channel, bridge, and mqtt data taken straight from ulnoiot.conf
+
+    # start accesspoint and mqtt_broker
+    tmux new-session -n AP -s UIoTSvrs ulnoiot exec accesspoint \; new-window -n MQTT ulnoiot exec mqtt_broker
 
 fi # accesspoint check
-
-
