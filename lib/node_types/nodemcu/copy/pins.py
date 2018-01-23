c5e9d77c94f155eead9e9da543db4f487f5b275d
Ulrich Norbisrath <devel@mail.ulno.net>
2018-01-22 22:26:28 -0500

    initial nodemcu support

---
+++ lib/node_types/nodemcu/copy/pins.py
@@ -0,0 +1,18 @@
+import machine
+Pin = machine.Pin
+
+d0 = Pin(16)
+d1 = Pin(5)
+d2 = Pin(4)
+d3 = Pin(0)
+d4 = Pin(2)
+tx = Pin(1)
+rx = Pin(3)
+onboardled = d4
+d5 = Pin(14)
+d6 = Pin(12)
+d7 = Pin(13)
+d8 = Pin(15)
+sd2 = Pin(9)
+sd3 = Pin(10)
+a0 = machine.ADC(0)
