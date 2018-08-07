39476ab0bac90bb95e0e434b6929e642a725cd72
Ulrich Norbisrath <devel@mail.ulno.net>
2018-08-07 00:25:01 -0300

    pins for sonoff and nodemcu

---
+++ lib/node_types/sonoff/src/pins.h
@@ -0,0 +1,45 @@
+// Most are in pins_arduino.h
+// Pins on SONOFF
+
+// d0 = Pin(16)
+
+// d1 = Pin(5)
+#define RELAIS2 D1
+#define REDLED2 D1
+
+// d2 = Pin(4)
+#define RELAIS = D2
+#define REDLED3 = D1
+
+// d3 = Pin(0)
+#define BUTTON1 = D3
+#define FLASHBUTTON = D3
+
+// d4 = Pin(2)
+#define USER2 = D4
+
+// tx = Pin(1)
+// rx = Pin(3)
+
+// d5 = Pin(14)
+#define USER1 = D5
+#define BUTTON4 = D5
+
+// d6 = Pin(12)
+#define RELAIS1 = D6
+#define REDLED1 = D6
+
+// d7 = Pin(13)
+#define ONBOARDLED = D7
+#define GREENLED = D7
+#define BLUELED = D7
+
+// d8 = Pin(15)
+#define RELAIS4 = D8
+#define REDLED4 = D8
+
+#define D9 9
+#define BUTTON2 = D9
+
+#define D10 10
+#define BUTTON3 = D10
--- lib/node_types/wemos_d1_mini/src/pins.h
+++ lib/node_types/wemos_d1_mini/src/pins.h
@@ -1,28 +1,33 @@
+// Most are in pins_arduino.h
 // Pins on Wemos D1 Mini
 
-#define Pin(p) p
+//#define Pin(p) p
 
-//// left (antenna front and up, usb back down)
-// RST
-// a0 machine.ADC(0)
-#define d0 Pin(16)
-#define d5 Pin(14)
-#define clk d5
-#define d6 Pin(12)
-#define miso d6
-#define d7 Pin(13)
-#define mosi d7
-#define d8 Pin(15)
-#define cs d8
-// 3.3V
+// //// left (antenna front and up, usb back down)
+// // RST
+// // a0 machine.ADC(0)
+// #define d0 Pin(16)
+#define WAKE D0
+// #define d5 Pin(14)
+#define CLK D5
+// #define d6 Pin(12)
+#define MISO D6
+// #define d7 Pin(13)
+#define MOSI D7
+// #define d8 Pin(15)
+#define CS D8
+// // 3.3V
 
-//// right
-#define tx Pin(1)
-#define rx Pin(3)
-#define d1 Pin(5)
-#define d2 Pin(4)
-#define d3 Pin(0)
-#define d4 Pin(2)
-#define onboardled d4
-// GND
-// 5V
+// //// right
+// #define tx Pin(1)
+// #define rx Pin(3)
+// #define d1 Pin(5)
+// #define SCL d1
+// #define d2 Pin(4)
+// #define SDA d2
+// #define d3 Pin(0)
+#define FLASHBUTTON D3
+// #define d4 Pin(2)
+#define ONBOARDLED D4
+// // GND
+// // 5V
