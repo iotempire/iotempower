a7d6f3849c26ff06f60c9224b87e0a1ddf19e8d7
Ulrich Norbisrath <devel@mail.ulno.net>
2018-08-08 14:39:38 -0300

    initial (non-working) support for esp-m

---
+++ lib/node_types/esp-m/src/pins.h
@@ -0,0 +1,33 @@
+// Most are in pins_arduino.h
+// Pins on Wemos D1 Mini
+
+//#define Pin(p) p
+
+// //// left (antenna front and up, usb back down)
+// // RST
+// // a0 machine.ADC(0)
+// #define d0 Pin(16)
+#define WAKE D0
+// #define d5 Pin(14)
+#define CLK D5
+// #define d6 Pin(12)
+#define MISO D6
+// #define d7 Pin(13)
+#define MOSI D7
+// #define d8 Pin(15)
+#define CS D8
+// // 3.3V
+
+// //// right
+// #define tx Pin(1)
+// #define rx Pin(3)
+// #define d1 Pin(5)
+// #define SCL d1
+// #define d2 Pin(4)
+// #define SDA d2
+// #define d3 Pin(0)
+#define FLASHBUTTON D3
+// #define d4 Pin(2)
+#define ONBOARDLED D4
+// // GND
+// // 5V
