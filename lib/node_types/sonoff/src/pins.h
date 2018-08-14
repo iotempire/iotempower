3760b8215a2e2503d93995661bc2b2256e1b2ae0
Ulrich Norbisrath <devel@mail.ulno.net>
2018-08-14 12:27:24 -0300

    sonoff pins update

--- lib/node_types/sonoff/src/pins.h
+++ lib/node_types/sonoff/src/pins.h
@@ -4,39 +4,39 @@
 // d0 = Pin(16)
 
 // d1 = Pin(5)
-#define RELAIS2 D1
-#define REDLED2 D1
+#define RELAIS2 5
+#define REDLED2 5
 
 // d2 = Pin(4)
-#define RELAIS = D2
-#define REDLED3 = D1
+#define RELAIS = 4
+#define REDLED3 = 4
 
 // d3 = Pin(0)
-#define BUTTON1 = D3
-#define FLASHBUTTON = D3
+#define BUTTON1 = 0
+#define FLASHBUTTON = 0
 
 // d4 = Pin(2)
-#define USER2 = D4
+#define USER2 = 2
 
 // tx = Pin(1)
 // rx = Pin(3)
 
 // d5 = Pin(14)
-#define USER1 = D5
-#define BUTTON4 = D5
+#define USER1 = 14
+#define BUTTON4 = 14
 
 // d6 = Pin(12)
-#define RELAIS1 = D6
-#define REDLED1 = D6
+#define RELAIS1 = 12
+#define REDLED1 = 12
 
 // d7 = Pin(13)
-#define ONBOARDLED = D7
-#define GREENLED = D7
-#define BLUELED = D7
+#define ONBOARDLED = 13
+#define GREENLED = 13
+#define BLUELED = 13
 
 // d8 = Pin(15)
-#define RELAIS4 = D8
-#define REDLED4 = D8
+#define RELAIS4 = 15
+#define REDLED4 = 15
 
 #define D9 9
 #define BUTTON2 = D9
