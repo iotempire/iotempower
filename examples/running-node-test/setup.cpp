14aa846328c2bd09f18f9e3d1c66c5d77e63b018
Ulrich Norbisrath <devel@mail.ulno.net>
2018-09-14 13:43:18 -0300

    make iotempire default password

--- examples/running-node-test/setup.cpp
+++ examples/running-node-test/setup.cpp
@@ -10,7 +10,7 @@
 
 //////// Device setup  ////////
 // output example
-// output(blue_led, "blue", ONBOARDLED, "off", "on").set("off");
+output(blue_led, "blue", ONBOARDLED, "off", "on").set("on");
 // void blink() {
 //     blue_led.toggle();
 //     do_later(2000, blink);
@@ -141,23 +141,23 @@
 //     } );
 
 // Examples for displays
-display(display1, "d1", font_medium).i2c(D6, D5);
+//display(display1, "d1", font_medium).i2c(D6, D5);
 //display44780(display2, "d2", 16, 2);
 
 // Example for pulse width modulation
 // pwm(blue_led, "blue", ONBOARDLED).with_frequency(1000);
 
 // Example for gyro
-gyro6050(gyro0, "g0").with_filter( [&] { // fuse accel value into one
-    // ignore angles
-    gyro0.measured_value(0).clear();
-    int a,b,c;
-    if(gyro0.measured_value(1).scanf("%d,%d,%d", &a, &b, &c)!=3)
-        return false;
-    unsigned long d = sqrt((unsigned long)(a*a) + (b*b) + (c*c)) + 0.5;
-    gyro0.measured_value(1).from(d>27?1:0);
-    return true;
-});
+//gyro6050(gyro0, "g0").with_filter( [&] { // fuse accel value into one
+//    // ignore angles
+//    gyro0.measured_value(0).clear();
+//    int a,b,c;
+//    if(gyro0.measured_value(1).scanf("%d,%d,%d", &a, &b, &c)!=3)
+//        return false;
+//    unsigned long d = sqrt((unsigned long)(a*a) + (b*b) + (c*c)) + 0.5;
+//    gyro0.measured_value(1).from(d>27?1:0);
+//    return true;
+//});
 
 
 // Optional init function, which is called right before device initialization
