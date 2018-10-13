aec223463cc7fd22d1dcaba1a454962a8192895f
Ulrich Norbisrath <devel@mail.ulno.net>
2018-10-13 10:06:39 -0300

    forgot backslash to execute on e command

--- lib/system_boot/raspi-boot.sh
+++ lib/system_boot/raspi-boot.sh
@@ -31,7 +31,7 @@ if [[ "ULNOIOT_AP_PASSWORD" ]]; then # pw was given, so start an accesspoint
             new-window -d -n MQTT  \
                 "$ULNOIOT_ROOT/run" exec mqtt_broker \; \
             new-window -d -n nodered  \
-                su - pi -c "ulnoiot exec node-red"
+                su - pi -c "ulnoiot exec node-red" \
             new-window -d -n cloudcmd  \
                 su - pi -c "ulnoiot exec cloudcmd --prefix cloudcmd --no-contact"
     ) &
