a3997204aa59e8137ad5619451bfab763f96b60a
Ulrich Norbisrath <devel@mail.ulno.net>
2018-10-18 01:05:24 -0300

    process more commands

--- examples/integriot/artificial-window/integriot/run.py
+++ examples/integriot/artificial-window/integriot/run.py
@@ -3,6 +3,7 @@
 
 from neopixel import * # led strip code by https://github.com/jgarff/rpi_ws281x
 
+import re
 
 LED_COUNT = 90
 LED_GPIO = 21
@@ -14,17 +15,19 @@ LED_CHANNEL = 0
 LED_STRIP_TYPE = ws.WS2811_STRIP_BRG
 strip = Adafruit_NeoPixel(LED_COUNT, LED_GPIO, LED_FREQ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP_TYPE)
 
+# Lines need to be swapped as the aluminium profiles angles the strips
+# into opposite directions
 matrix_map = [
-[ 72, 73, 74, 75, 76, 77, 78, 79, 80 ],
-[ 89, 88, 87, 86, 85, 84, 83, 82, 81 ],
-[ 54, 55, 56, 57, 58, 59, 60, 61, 62 ],
-[ 71, 70, 69, 68, 67, 66, 65, 64, 63 ],
-[ 36, 37, 38, 39, 40, 41, 42, 43, 44 ],
-[ 53, 52, 51, 50, 49, 48, 47, 46, 45 ],
-[ 18, 19, 20, 21, 22, 23, 24, 25, 26 ],
-[ 35, 34, 33, 32, 31, 30, 29, 28, 27 ],
-[  0,  1,  2,  3,  4,  5,  6,  7,  8 ],
-[ 17, 16, 15, 14, 13, 12, 11, 10,  9 ]
+    [ 72, 73, 74, 75, 76, 77, 78, 79, 80 ],
+    [ 89, 88, 87, 86, 85, 84, 83, 82, 81 ],
+    [ 54, 55, 56, 57, 58, 59, 60, 61, 62 ],
+    [ 71, 70, 69, 68, 67, 66, 65, 64, 63 ],
+    [ 36, 37, 38, 39, 40, 41, 42, 43, 44 ],
+    [ 53, 52, 51, 50, 49, 48, 47, 46, 45 ],
+    [ 18, 19, 20, 21, 22, 23, 24, 25, 26 ],
+    [ 35, 34, 33, 32, 31, 30, 29, 28, 27 ],
+    [  0,  1,  2,  3,  4,  5,  6,  7,  8 ],
+    [ 17, 16, 15, 14, 13, 12, 11, 10,  9 ]
 ]
 
 MATRIX_HEIGHT=len(matrix_map)
@@ -73,66 +76,84 @@ def animation_next():
     if animation:
         animation()
 
-def bright( _ ):
-    animation_stop()
-    fill(Color(255,255,255))
-    show()
+color_map = {
+    "bright": Color(255,255,255),
+    "black" : Color(0,0,0),
+    "off"   : Color(0,0,0),
+    "white" : Color(255,180,130),
+    "on"    : Color(255,180,130),
+    "red"   : Color(255,0,0),
+    "green" : Color(0,255,0),
+    "blue"  : Color(0,0,255),
+    "yellow": Color(255,50,10),
+}
 
-def black( _ ):
-    animation_stop()
-    clear()
-    show()
+def set_color(color, x=-1, y=-1):
+    if isinstance(color, str):
+        color = color.lower()
+        if color in color_map:
+            color = color_map[color]
+        else:
+            rgb = color.split(",")
+            if len(rgb) == 3: # triple?
+                try:
+                    r = int(rgb[0])
+                    g = int(rgb[1])
+                    b = int(rgb[2])
+                except:
+                    pass
+                else:
+                    color = Color(r,g,b)
+            else:
+                if(len(color) == 6): # 6 digit hex?
+                    try:
+                        rgb = int("0x"+color)
+                    except:
+                        pass
+                    else:
+                        b = rgb & 255
+                        rgb //=  256
+                        g = rgb & 255
+                        rgb //= 256
+                        r = rgb
+                        color = Color(r,g,b)
+    if isinstance(color, Color):
+        if x == -1 || y == -1:
+            fill(color)
+        else:
+            set_pixel(x, y, color)
 
-def natural_white( _ ):
-    animation_stop()
-    fill(Color(255,180,130))
-    # better?
-    # fill(Color(255,120,50))
-    show()
 
-def red( _ ):
-    animation_stop()
-    fill(Color(255,0,0))
-    show()
-
-def green( _ ):
-    animation_stop()
-    fill(Color(0,255,0))
-    show()
-
-def blue( _ ):
-    animation_stop()
-    fill(Color(0,0,255))
-    show()
-
-def yellow( _ ):
-    animation_stop()
-    fill(Color(255,50,5))
-    show()
 
 
 command_list = {
-    "bright": bright,
-    "black": black,
-    "off": black,
-    "white": natural_white,
-    "on": natural_white,
-    "red": red,
-    "green": green,
-    "blue": blue,
-    "yellow": yellow,
+# "command":method
 }
 
 def matrix_cb(msg):
+    animation_stop()
     # Accept commands to change what is displayed on the matrix
-
     command=msg.split()
     print("received: ", command)
-    if len(command)>0:
+    while len(command)>0:
         first = command[0].lower()
         if first in command_list:
-            command_list[first](command[1:])
-
+            del command[0]
+            processed = command_list[first](command)
+            command = command[processed:]
+        else: # color with coordinates given?
+            x = -1
+            y = -1
+            try:
+                x = int(command[0])
+                y = int(command[1])
+                del command[0]
+                del command[0]
+            except:
+                pass
+            set_color(command[0], x, y)
+            del command[0]
+    show()
 
 
 # init and run integriot
