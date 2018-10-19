3ce581dfd629ff0653481cc5684381b8541b7e22
Ulrich Norbisrath <devel@mail.ulno.net>
2018-10-19 02:04:52 -0300

    access folder

--- examples/integriot/artificial-window/integriot/run.py
+++ examples/integriot/artificial-window/integriot/run.py
@@ -244,7 +244,7 @@ def command_images(commands):
     folder = commands[0]
     imagelist=[]
     for f in os.listdir(folder):
-        imagelist.append(Image.open(f))
+        imagelist.append(Image.open(os.path.join(folder, f)))
     animation_frames = len(imagelist)+1  # we will keep this higher to refill when down
     return 1 # consume command
 
