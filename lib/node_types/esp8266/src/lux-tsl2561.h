30c8173f534d720e884ae8316a36b474dd196b22
Ulrich Norbisrath <devel@mail.ulno.net>
2018-12-10 16:01:31 +0100

    missing lux-files

---
+++ lib/node_types/esp8266/src/lux-tsl2561.h
@@ -0,0 +1,25 @@
+// lux-tsl2561.h
+// light sensor based on tsl2561 i2c chip
+
+#ifndef _ULNOIOT_TSL2561_H_
+#define _ULNOIOT_TSL2561_H_
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <i2c_device.h>
+
+#include <SparkFunTSL2561.h>
+
+class Lux_TSL2561 : public I2C_Device {
+    private:
+        SFE_TSL2561 *sensor = NULL;
+        bool _gain;
+        unsigned long last_read;
+        int discardval = 5;
+    public:
+        Lux_TSL2561(const char* name, bool gain=false);
+        void start();
+        bool measure();
+};
+
+#endif // _ULNOIOT_BH1750_H_
