f1ddbd7110513369d71f87379309ee12530c7763
Ulrich Norbisrath <devel@mail.ulno.net>
2019-06-03 18:12:13 -0300

    combine nodemcu boards

--- lib/node_types/nodemcu/src/pins.h
+++ lib/node_types/nodemcu/src/pins.h
@@ -1,4 +1,42 @@
 // Most are in pins_arduino.h
+// Pins on Wemos D1 Mini
+
+//#define Pin(p) p
+
+
+// Wemos D1 mini
+// //// left (antenna front and up, usb back down)
+// // RST
+// // A0 ADC 0
+// #define D0 16
+#define WAKE D0
+// #define D5 14
+#define CLK D5
+// #define D6 12
+#define MISO D6
+// #define D7 13
+#define MOSI D7
+// #define D8 15
+#define CS D8
+// // 3.3V
+
+// //// right
+// #define TX 1
+// #define RX 3
+// #define D1 5
+// #define SCL D1
+// #define D2 4
+// #define SDA D2
+// #define D3 0
+#define FLASHBUTTON D3
+// #define D4 2
+#define ONBOARDLED D4
+// // GND
+// // 5V
+
+
+//// Nodemcu
+// Most are in pins_arduino.h
 // Pins on ESP-M Nodemcu board
 // based on:
 // https://samnicholls.net/wp-content/uploads/2016/11/esp8266_devkit_horizontal-01.png
@@ -23,20 +61,20 @@
 // d2 = Pin(4)
 // sda = d2
 // d3 = Pin(0)
-#define FLASHBUTTON D3
+//#define FLASHBUTTON D3
 // d4 = Pin(2)
-#define ONBOARDLED D4
+//#define ONBOARDLED D4
 // 3.3V
 // GND
 // d5 = Pin(14)
-#define CLK D5
+//#define CLK D5
 // d6 = Pin(12)
-#define MISO D6
+//#define MISO D6
 // miso = d6
 // d7 = Pin(13)
-#define MOSI D7
+//#define MOSI D7
 // d8 = Pin(15)
-#define CS D8
+//#define CS D8
 // rx = Pin(3)
 // tx = Pin(1)
 // GND
@@ -47,9 +85,9 @@
 // a0 = machine.ADC(0)
 // reserved
 // reserved
-#define D10 10
+// #define D10 10
 #define SD3 = D10
 #define SDD3 = S10
-#define D9 9
+// #define D9 9
 #define SD2 = D9
 #define SDD2 = D9
--- lib/node_types/wemos_d1_mini/pre-flash/setup.cpp
+++
@@ -1,2 +0,0 @@
-output(blue, ONBOARDLED, "off", "on").set("on");
-input(button1, D3, "released", "pressed").with_threshold(3);
