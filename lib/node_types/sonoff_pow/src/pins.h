2f64079e7c0767f5d957cea5590ef44185b1c28f
Ulrich Norbisrath <devel@mail.ulno.net>
2019-07-26 14:55:01 -0300

    initial support for sonoff pow

---
+++ lib/node_types/sonoff_pow/src/pins.h
@@ -0,0 +1,26 @@
+// Most are in pins_arduino.h
+// Pins on SONOFF-POW
+
+// d3 Pin(0)
+#define BUTTON1 0
+#define FLASHBUTTON 0
+
+// d4 Pin(2)
+#define USER2 2
+
+// d8 Pin(15)
+#define ONBOARDLED 15
+#define GREENLED 15
+#define BLUELED 15
+
+// power measurement over rx/tx pins TODO: implement
+// tx Pin(1)
+// rx Pin(3)
+
+// d5 Pin(14)
+#define USER1 14
+
+// d6 Pin(12)
+#define RELAIS1 12
+#define RELAIS 12
+#define REDLED1 12
