19a9b9a26a63b7c573bff596de0187a0c603c00f
Ulrich Norbisrath <devel@mail.ulno.net>
2020-02-17 15:50:48 -0300

    support wroom 02

---
+++ lib/node_types/wroom_02/src/pins.h
@@ -0,0 +1,33 @@
+// Most are in pins_arduino.h
+// Pins on Wroom 02
+// // A0 ADC 0
+#define D0 16
+#define WAKE D0
+#define D5 14
+#define CLK D5
+#define D6 12
+#define MISO D6
+#define D7 13
+#define MOSI D7
+#define D8 15
+#define CS D8
+#define SS D8 // or sometimes SDA (for example on RFID-RC522)
+
+#define TX 1
+#define RX 3
+#define D1 5
+#define SCL D1
+#define D2 4
+#define SDA D2
+#define D3 0
+#define FLASHBUTTON D3
+#define D4 2
+#define ONBOARDLED D0
+
+
+#define D10 10
+#define SD3 D10
+#define SDD3 S10
+#define D9 9
+#define SD2 D9
+#define SDD2 D9
