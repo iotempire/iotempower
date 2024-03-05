fbc73344bd75471f074f079d96cf7cfde76249b7
Ulrich Norbisrath <devel@mail.ulno.net>
2024-03-06 01:33:01 +0200

    refactor test to check for installed binaries nto os specific packages

--- tests/test_installations.py
+++ tests/test_installations.py
@@ -15,25 +15,25 @@ with open(installation_options_file, "r") as f:
     installation_options = json.load(f)
 
 packages = [
-    {"name": "python3.11-venv", "package_manager": "apt", "module": "general"},
-    {"name": "git", "package_manager": "apt", "module": "general"},
-    {"name": "jq", "package_manager": "apt", "module": "general"},
-    {"name": "make", "package_manager": "apt", "module": "general"},
-    {"name": "curl", "package_manager": "apt", "module": "general"},
-    {"name": "nodejs", "package_manager": "apt", "module": "general"},
-    {"name": "haveged", "package_manager": "apt", "module": "general"},
-    {"name": "python3-dev", "package_manager": "apt", "module": "general"},
+    {"name": "virtualenv", "package_manager": "binary", "module": "general"},
+    {"name": "git", "package_manager": "binary", "module": "general"},
+    {"name": "jq", "package_manager": "binary", "module": "general"},
+    {"name": "make", "package_manager": "binary", "module": "general"},
+    {"name": "curl", "package_manager": "binary", "module": "general"},
+    {"name": "nodejs", "package_manager": "binary", "module": "general"},
+    {"name": "haveged", "package_manager": "binary", "module": "general"},
+    {"name": "python3-dev", "package_manager": "binary", "module": "general"},
     {"name": "terminal-kit", "package_manager": "npm", "module": "general"},
-    {"name": "g++", "package_manager": "apt", "module": "cloud_commander"},
+    {"name": "g++", "package_manager": "binary", "module": "cloud_commander"},
     {"name": "gritty", "package_manager": "npm", "module": "cloud_commander"},
     {"name": "cloudcmd", "package_manager": "npm", "module": "cloud_commander"},
     {"name": "node-red", "package_manager": "npm", "module": "node_red"},
-    {"name": "debian-keyring", "package_manager": "apt", "module": "caddy"},
-    {"name": "apt-transport-https", "package_manager": "apt", "module": "caddy"},
-    {"name": "debian-archive-keyring", "package_manager": "apt", "module": "caddy"},
-    {"name": "caddy", "package_manager": "apt", "module": "caddy"},
-    {"name": "mosquitto-clients", "package_manager": "apt", "module": "mosquitto"},
-    {"name": "mosquitto", "package_manager": "apt", "module": "mosquitto"},
+    {"name": "debian-keyring", "package_manager": "binary", "module": "caddy"},
+    {"name": "apt-transport-https", "package_manager": "binary", "module": "caddy"},
+    {"name": "debian-archive-keyring", "package_manager": "binary", "module": "caddy"},
+    {"name": "caddy", "package_manager": "binary", "module": "caddy"},
+    {"name": "mosquitto-clients", "package_manager": "binary", "module": "mosquitto"},
+    {"name": "mosquitto", "package_manager": "binary", "module": "mosquitto"},
 ]
 
 packages_to_test = []
@@ -44,11 +44,11 @@ for key, value in installation_options.items():
 
 @pytest.mark.parametrize("package_name, package_manager, module", packages_to_test)
 def test_eval(package_name, package_manager, module) -> None:
-    if package_manager == "apt":
-        command = f"dpkg -s {package_name}"
+    if package_manager == "binary":
+        command = f"which {package_name}"
     elif package_manager == "npm":
         command = f"cd {local_dir}/nodejs && npm list {package_name}"
     else:
-        raise NotImplementedError("Only apt and npm packages are supported for now")
+        raise NotImplementedError("Only installed binaries and npm packages are supported for now")
     result = subprocess.run(command, shell=True)
-    assert result.returncode == 0, f"Cannot found {package_name} in your system which is need for {module}"
+    assert result.returncode == 0, f"Cannot find {package_name} in your system which is needed for {module}"
