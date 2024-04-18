a5a3a075381b35309ed266d9f4da74ea53e8ee96
mbz4 <matevz.zorec@gmail.com>
2024-04-18 20:32:18 +0300

    Fancy new i2c driver and a fix in bmp180, 280. Docs pending for sgp30.

---
+++ lib/node_types/esp/src/dev_sgp30.h
@@ -0,0 +1,20 @@
+// dev_sgp30.h
+// Header File for controlling the SGP30 IAQ sensor
+
+#ifndef _IOTEMPOWER_SGP30_H_
+#define _IOTEMPOWER_SGP30_H_
+
+#include <i2c-device.h>
+#include "sensirion_common.h"
+#include <sgp30.h>
+
+class Sgp30 : public I2C_Device {
+    private:
+        bool _in_init_phase = false;
+    public:
+        Sgp30(const char* name);
+        void i2c_start();
+        bool measure();
+};
+
+#endif // _IOTEMPOWER_SGP30_H_
