1e990462d0f95901a181f6e9e02be3aebf511437
mbz4 <matevz.zorec@gmail.com>
2024-05-21 14:01:55 +0300

    trying to fix scd4x driver compile errors...

--- lib/node_types/esp/src/dev_scd4x.h
+++ lib/node_types/esp/src/dev_scd4x.h
@@ -1,18 +1,20 @@
+// dev_scd4x.h
+// Header File for controlling the SCD4X CO2 sensors
+
 #ifndef _IOTEMPOWER_SCD4X_H_
 #define _IOTEMPOWER_SCD4X_H_
 
-#include <i2c-device.h>
+#include <i2c-device.h>  // Include the I2C device library
 #include "DFRobot_SCD4X.h"  // Include the SCD4X library
 
-class SCD4X_Device : public I2C_Device {
-private:
-    DFRobot_SCD4X scd4x;  // SCD4X sensor object
-    bool _in_init_phase = true;
+class Scd4x : public I2C_Device {
+   private:
+    bool _in_init_phase = false;
 
-public:
-    SCD4X_Device(const char* name, TwoWire &wire = Wire, uint8_t i2cAddr = SCD4X_I2C_ADDR);
+   public:
+    Scd4x(const char* name);
     void i2c_start();
     bool measure();
 };
 
-#endif // _IOTEMPOWER_SCD4X_H_
+#endif  // _IOTEMPOWER_SCD4X_H_
