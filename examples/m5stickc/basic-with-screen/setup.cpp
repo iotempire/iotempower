c3e8738a4dec2ad7040bffaf8aede6179d081d84
Ulrich Norbisrath <devel@mail.ulno.net>
2024-06-22 01:55:10 +0300

    m5stick initial imu support

--- examples/m5stickc/basic-with-screen/setup.cpp
+++ examples/m5stickc/basic-with-screen/setup.cpp
@@ -4,6 +4,7 @@ out(led, ONBOARDLED).inverted().off();
 button(home, BUTTON_HOME, "pressed", "released").inverted().debounce(10);
 button(buttom, BUTTON_RIGHT, "pressed", "released").inverted().debounce(10);
 m5stickc_display(console, 1, 270); // why does lcd as name turn into Lcd?
+m5stickc_imu(imu, true, true, false, false);
 
 void start() {
         do_later(100, [] () {
