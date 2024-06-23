d05d24b5ee08a40a2571502efd095bb89eeddc60
Araz Heydarov <arazheyderov@gmail.com>
2024-06-23 17:03:38 +0300

    new devices and fixes added to compilation tests

--- tests/test_compilation.py
+++ tests/test_compilation.py
@@ -6,7 +6,6 @@ import pytest
 
 from tests.data import isolated_combinations_to_test
 
-# TODO find platformio installation and move it to the requirements.txt -> Platformio is already in requirement.txt
 local_dir = os.getenv("IOTEMPOWER_LOCAL", "")
 test_dir = os.path.join(local_dir, ".tests", "test_compilation")
 cache_dir = os.path.join(local_dir, "compile_cache", "platformio")
@@ -19,9 +18,6 @@ open(f"{test_dir}/system.conf", "w").close()
 remove_cache_flag = os.getenv("IOTEMPOWER_TEST_REMOVE_CACHE", "")
 
 
-# TODO
-# Node name should be randomized hash
-# Run with multiprocessing https://stackoverflow.com/questions/29560212/parallely-running-parameterized-tests-in-pytest
 @pytest.mark.parametrize("board_name, device_name, example_syntax", isolated_combinations_to_test)
 def test_compilation_isolated(board_name: str, device_name: str, example_syntax: str):
     node_directory = f"{test_dir}/node"
@@ -39,7 +35,3 @@ def test_compilation_isolated(board_name: str, device_name: str, example_syntax:
         if remove_cache_flag.lower() == "true":
             shutil.rmtree(cache_dir, ignore_errors=True)
 
-
-# def test_compilation_integrated():
-#     cluster_folder = f"{test_dir}/cluster"
-#     os.makedirs(cluster_folder, exist_ok=True)
